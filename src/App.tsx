82e570a11f8069c1f1b045540ee8feb43e65bba4
phongduong-dp <duonghongphong.dp@gmail.com>
2022-12-13 22:53:08 +0700

    Install and configuration for Taillwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
+import {FiSettings} from 'react-icons/fi';
 
 import './App.css'
 
@@ -5,10 +8,7 @@ function App() {
 
   return (
     <div>
-      <h1>Welcome to FinDas Project</h1>
-      <h2>Create by:</h2>
-      <h3>Phong Duong</h3>
-      <h3>Phuoc Hieu</h3>
+      <h1 className='underline text-3xl'>Welcome to FinDas Project</h1>
     </div>
   )
 }
