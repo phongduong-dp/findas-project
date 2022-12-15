6792e7254c9e8b60e7eff9d058a29625b3a2eb62
phongduong-dp <duonghongphong.dp@gmail.com>
2022-12-15 22:58:45 +0700

    Adding license for SyncFusion

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
